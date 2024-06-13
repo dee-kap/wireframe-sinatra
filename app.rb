42c9bf83ddb35bd4ac1113f3b1a8d6a344649056
deepak <dee-kap@users.noreply.github.com>
2024-06-13 12:21:51 +1000

    Initial commit
    Feature complete

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+post '/subscribe' do
+  @name = params['name']
+  response.headers['Content-Type'] = 'text/vnd.turbo-stream.html'
+  erb :subscribe
+end
